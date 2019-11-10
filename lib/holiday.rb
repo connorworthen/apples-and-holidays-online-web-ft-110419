898bb2da1575c871769abe13514e98683b913c7a
connor worthen <connorw220@protonmail.com>
2019-11-10 23:25:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,9 +17,9 @@ def second_supply_for_fourth_of_july(holiday_hash)
      }
    }
 
-   holiday_hash[:summer][:fourth_of_july][1]
+   
 
-  # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][0]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
